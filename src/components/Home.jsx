45dcebd99e249e4bb0f976e5c4fe031f97d39ef1
Om117 <omjadav04@gmail.com>
2023-11-10 00:31:37 +0530

    added Home and Footer

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-center bg-gray-400">
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default Home;
